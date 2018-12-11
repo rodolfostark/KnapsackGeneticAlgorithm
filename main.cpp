f17515e5b1bf8e29c5a801ac040ca120be1c4b86
rodolfostark <rodolfostark@ufrn.edu.br>
2018-12-10 23:16:20 -0300

    Desenvolvendo o calculo do fitness.

--- main.cpp
+++ main.cpp
@@ -11,8 +11,6 @@
 #define POPULACAO_MAX 100
 #define MAX_ITENS 20
 
-
-
 using namespace std;
 
 int main()
@@ -25,55 +23,51 @@ int main()
     int qt_config = 0;
     int qt_itens = 0;
 
-    fstream arquivoConfig;
+    ifstream arquivoConfig;
     arquivoConfig.open("/home/rodolfo/Documentos/C&T - COMPUTAÇÃO/4º SEMESTRE/TÓPICOS AVANÇADOS EM INFORMÁTICA I/BagGA/config.txt");
-    if(arquivoConfig.is_open())
+    while(arquivoConfig.good())
     {
-        while(arquivoConfig.good())
-        {
-            ConfigBagString config_bag;
-            getline(arquivoConfig,config_bag.peso_suportado,',');
-            getline(arquivoConfig,config_bag.populacao,',');
-            getline(arquivoConfig,config_bag.qt_itens,',');
-            getline(arquivoConfig,config_bag.timeout_sec,'\n');
-            config[qt_itens].peso_suportado = stof(config_bag.peso_suportado.c_str());
-            config[qt_itens].populacao = stoi(config_bag.populacao.c_str());
-            config[qt_itens].qt_itens = stoi(config_bag.qt_itens.c_str());
-            config[qt_itens].timeout_sec = stoi(config_bag.timeout_sec.c_str());
-            qt_itens++;
-        }
-        arquivoConfig.close();
+        ConfigBagString config_bag;
+        getline(arquivoConfig,config_bag.peso_suportado,',');
+        getline(arquivoConfig,config_bag.populacao,',');
+        getline(arquivoConfig,config_bag.qt_itens,',');
+        getline(arquivoConfig,config_bag.timeout_sec,'\n');
+        config[qt_config].peso_suportado = atof(config_bag.peso_suportado.c_str());
+        config[qt_config].populacao = atoi(config_bag.populacao.c_str());
+        config[qt_config].qt_itens = atoi(config_bag.qt_itens.c_str());
+        config[qt_config].timeout_sec = atoi(config_bag.timeout_sec.c_str());
+        qt_config++;
     }
-    else
-    {
+    if(!arquivoConfig.is_open()){
         cout << "ERRO AO CARREGAR AS CONFIGURAÇÕES!" << endl;
     }
-    fstream arquivoItens;
-    arquivoItens.open("/home/rodolfo/Documentos/C&T - COMPUTAÇÃO/4º SEMESTRE/TÓPICOS AVANÇADOS EM INFORMÁTICA I/BagGA/lista_itens.txt");
-    if(arquivoItens.is_open()){
-        while(arquivoItens.good()){
-            ItemString item_string;
-            getline(arquivoItens,item_string.nome,',');
-            getline(arquivoItens,item_string.beneficio,',');
-            getline(arquivoItens,item_string.peso,'\n');
-            itens[qt_itens-3].nome = item_string.nome;
-            itens[qt_itens-3].beneficio = stoi(item_string.beneficio.c_str());
-            itens[qt_itens-3].peso = stof(item_string.peso.c_str());
-            qt_itens++;
-        }
-        //finalizando o vetor ja que nao está usando o push
-        itens[qt_itens].nome += '\0';
-        arquivoItens.close();
-        cout << "Deu bom" << endl;
-    }
     else{
-        cout<< "ERRO AO CARREGAR OS ITENS!";
+        cout << "CONFIGURAÇÕES CARREGADAS COM SUCESSO." << endl;
+        arquivoConfig.close();
+    }
+    ifstream arquivoItens;
+    arquivoItens.open("/home/rodolfo/Documentos/C&T - COMPUTAÇÃO/4º SEMESTRE/TÓPICOS AVANÇADOS EM INFORMÁTICA I/BagGA/lista_itens.txt");
+    while(arquivoItens.good()){
+        ItemString item_string;
+        getline(arquivoItens,item_string.nome,',');
+        getline(arquivoItens,item_string.beneficio,',');
+        getline(arquivoItens,item_string.peso,'\n');
+        itens[qt_itens].nome = item_string.nome;
+        itens[qt_itens].beneficio = atoi(item_string.beneficio.c_str());
+        itens[qt_itens].peso = atof(item_string.peso.c_str());
+        qt_itens++;
+    }
+    if(!arquivoItens.is_open()){
+        cout<< "ERRO AO CARREGAR OS ITENS!" << endl;
+    }
+    else {
+        cout << "ITENS CARREGADOS COM SUCESSO." << endl;
+        arquivoItens.close();
     }
     imprimir_config(config[1]);
     cout << "Itens: "<< endl;
-    imprimir_itens(itens,config[1].qt_itens);
+    imprimir_itens(itens, config[1].qt_itens);
     //Até aqui é pré-processamento dos itens e possíveis configurações
-
     int numero_iteracoes = 0;
     vector<Mochila> populacao;
     for(int i = 0;i < POPULACAO_MAX; i++){
@@ -84,8 +78,8 @@ int main()
     }
     while(numero_iteracoes < 500){
         sort(populacao.begin(), populacao.end());
+        numero_iteracoes++;
     }
-
     return 0;
 }
 
