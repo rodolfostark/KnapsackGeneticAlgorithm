ac68429e4ff3229cc86e822289e82761e2cc5037
rodolfostark <rodolfostark@ufrn.edu.br>
2018-12-10 22:22:06 -0300

    Desenvolvendo o calculo do fitness.

--- main.cpp
+++ main.cpp
@@ -15,8 +15,8 @@
 #define POPULACAO_MAX 100
 #define MAX_ITENS 20
 
-void imprimir_itens(Item i[MAX_ITENS], unsigned tam);
-void imprimir_config(ConfigBag c);
+void imprimir_itens(Item itens[MAX_ITENS], int tamanho);
+void imprimir_config(ConfigBag configuracoes);
 
 using namespace std;
 
@@ -34,20 +34,16 @@ int main()
     {
         while(arquivoConfig.good())
         {
-            ConfigBagString t;
-
-            getline(arquivoConfig,t.peso_suportado,',');
-            getline(arquivoConfig,t.populacao,',');
-            getline(arquivoConfig,t.qt_itens,',');
-            getline(arquivoConfig,t.timeout_sec,'\n');
-
-            config[qt_itens].peso_suportado = stof(t.peso_suportado.c_str());
-            config[qt_itens].populacao = stoi(t.populacao.c_str());
-            config[qt_itens].qt_itens = stoi(t.qt_itens.c_str());
-            config[qt_itens].timeout_sec = stoi(t.timeout_sec.c_str());
-
+            ConfigBagString config_bag;
+            getline(arquivoConfig,config_bag.peso_suportado,',');
+            getline(arquivoConfig,config_bag.populacao,',');
+            getline(arquivoConfig,config_bag.qt_itens,',');
+            getline(arquivoConfig,config_bag.timeout_sec,'\n');
+            config[qt_itens].peso_suportado = stof(config_bag.peso_suportado.c_str());
+            config[qt_itens].populacao = stoi(config_bag.populacao.c_str());
+            config[qt_itens].qt_itens = stoi(config_bag.qt_itens.c_str());
+            config[qt_itens].timeout_sec = stoi(config_bag.timeout_sec.c_str());
             qt_itens++;
-
         }
         arquivoConfig.close();
     }
@@ -57,45 +53,35 @@ int main()
     }
 
     fstream arquivoItens("home/rodolfo/Documentos/C&T - COMPUTAÇÃO/4º SEMESTRE/TÓPICOS AVANÇADOS EM INFORMÁTICA I/BagGA/lista_itens.txt");
-    if(arquivoItens.is_open())
-    {
-        while(arquivoItens.good())
-        {
-            if(qt_itens==0)
-            {
+    if(arquivoItens.is_open()){
+        while(arquivoItens.good()){
+            if(qt_itens==0){
                 qt_itens++;
                 continue;
             }
-            else
-            {
-                ItemString i;
-
-                getline(arquivoItens,i.nome,',');
-                getline(arquivoItens,i.beneficio,',');
-                getline(arquivoItens,i.peso,'\n');
-
-                itens[qt_itens-3].nome = i.nome;
-                itens[qt_itens-3].beneficio = stoi(i.beneficio.c_str());
-                itens[qt_itens-3].peso = stof(i.peso.c_str());
+            else{
+                ItemString item_string;
+                getline(arquivoItens,item_string.nome,',');
+                getline(arquivoItens,item_string.beneficio,',');
+                getline(arquivoItens,item_string.peso,'\n');
+                itens[qt_itens-3].nome = item_string.nome;
+                itens[qt_itens-3].beneficio = stoi(item_string.beneficio.c_str());
+                itens[qt_itens-3].peso = stof(item_string.peso.c_str());
             }
-
             qt_itens++;
-
         }
         //finalizando o vetor ja que nao está usando o push
-        itens[qt_itens].nome='\0';
-
+        itens[qt_itens].nome += '\0';
         arquivoItens.close();
-
     }
     else
     {
         cout<<"ERRO AO CARREGAR OS ITENS!";
     }
-
     imprimir_config(config[1]);
     cout<<"itens:"<<endl;
     imprimir_itens(itens,config[1].qt_itens);
+    //Até aqui é pré-processamento dos itens e possíveis configurações
 
     int numero_iteracoes = 0;
     vector<Mochila> populacao;
@@ -105,25 +91,21 @@ int main()
     }
     while(numero_iteracoes < 500){
         sort(populacao.begin(), populacao.end());
-        //travei aqui!!!!
     }
 
     return 0;
 }
 
-void imprimir_config(ConfigBag c)
+void imprimir_config(ConfigBag configuracoes)
 {
-    cout<<"peso suportado pela mochila:"<<c.peso_suportado<<endl;
-    cout<<"numero da população utilizada no algoritmo:"<<c.populacao<<endl;
-    cout<<"quantidade de itens disponiveis para adicionar na mochila:"<<c.qt_itens<<endl;
-    cout<<"tempo limite em (s) de execução:"<<c.timeout_sec<<endl;
+    cout << "Peso suportado pela mochila: "<< configuracoes.peso_suportado<<endl;
+    cout << "Tamanho da população utilizada no algoritmo: "<< configuracoes.populacao<<endl;
+    cout << "Quantidade de itens disponiveis para adicionar na mochila: "<< configuracoes.qt_itens<<endl;
+    cout << "Tempo limite em (s) de execução: "<< configuracoes.timeout_sec<<endl;
 }
 
-void imprimir_itens(Item item[MAX_ITENS], unsigned tam){
-
-    for(unsigned i=0; i<tam; i++){
-
-        cout<<item[i].nome<<" / "<<item[i].beneficio<<" / "<<item[i].peso<<endl;
-
+void imprimir_itens(Item itens[MAX_ITENS], int tamanho){
+    for(int i = 0; i < tamanho; i++){
+        cout<< itens[i].nome << " / "<< itens[i].beneficio << " / " << itens[i].peso << endl;
     }
 }
